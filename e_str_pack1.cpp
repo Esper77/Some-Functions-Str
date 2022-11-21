fe393e7958d9ee1e17c0d0f37a8d9635b27207ca
Esper77 <112155793+Esper77@users.noreply.github.com>
2022-11-21 14:36:38 +0300

    Update e_str_pack1.cpp

--- e_str_pack1.cpp
+++ e_str_pack1.cpp
@@ -1,6 +1,20 @@
 #include <iostream>
+#include "str_easy.h"
 using namespace std;
 
 string itc_hello_str(string name){
-    
+    return "Hello, " + name;
+}
+
+long long itc_len(string str){
+    long long counter = 0;
+    for (long long i = 0; str[i] != '\0'; i++){
+    counter++;
+    }
+    return counter;
+}
+
+void itc_print_copy_str(string str, int number){
+    for (int i = 0; i < number; i++)
+        cout << str;
 }
