c4a6d8c31fbe41c71cb7368b19cf8913898f4e27
Esper77 <112155793+Esper77@users.noreply.github.com>
2022-11-28 15:04:11 +0300

    Update e_str_pack1.cpp

--- e_str_pack1.cpp
+++ e_str_pack1.cpp
@@ -1,20 +1,37 @@
-#include <iostream>
 #include "str_easy.h"
+#include <string>
 using namespace std;
 
-string itc_hello_str(string name){
-    return "Hello, " + name;
-}
+string itc_hello_str(string name) { return "Hello, " + name; }
 
-long long itc_len(string str){
-    long long counter = 0;
-    for (long long i = 0; str[i] != '\0'; i++){
+long long itc_len(string str) {
+  long long counter = 0;
+  for (int i = 0; str[i] != '\0'; i++) {
     counter++;
-    }
-    return counter;
+  }
+  return counter;
 }
-
-void itc_print_copy_str(string str, int number){
-    for (int i = 0; i < number; i++)
-        cout << str;
+void itc_print_copy_str(string str, int number) {
+  for (int i = 0; i < number; ++i)
+    cout << str;
+}
+void itc_first_end_three(string str) {
+  long long str_length = itc_len(str);
+  if (str_length > 5) {
+    cout << str[0] << str[1] << str[2] << str[str_length - 3]
+         << str[str_length - 2] << str[str_length - 1];
+  } else {
+    while (str_length > 0) {
+      cout << str[0];
+      str_length--;
+    }
+  }
+}
+int itc_count_char_in_str(char ch, string str) {
+  int counter = 0;
+  for (int i = 0; i <= itc_len(str); i++) {
+    if (ch == str[i])
+      counter++;
+  }
+  return counter;
 }
